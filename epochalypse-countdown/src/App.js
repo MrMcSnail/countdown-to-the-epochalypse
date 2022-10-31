ee210bcabde053d0d71e83636b7361bd332181a2
Kyle McPhail <99103416+MrMcSnail@users.noreply.github.com>
2022-10-31 23:36:46 +0000

    styles

--- epochalypse-countdown/src/App.js
+++ epochalypse-countdown/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import face from './face.png';
 import './App.css';
 import Countdown from './Countdown';
 
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <img src={face} className="App-face" alt="clock-face" />
       </header>
       <Countdown />
     </div>
